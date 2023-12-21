69f1a89552126e995afbb8dbb7373f2c53d267e9
Jeremy Lee <jeremy.lee1664@gmail.com>
2023-12-21 15:42:20 -0500

    changing session initiation to use the default keystone token endpoint

--- keystoneauth_openid/plugin.py
+++ keystoneauth_openid/plugin.py
@@ -19,12 +19,14 @@ import socket
 import webbrowser
 import os
 import json
+from urllib.parse import urlparse, parse_qs
 from keystoneauth1 import _utils as utils
 from keystoneauth1 import access
 from keystoneauth1.identity.v3 import oidc
 from positional import positional
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from keystoneauth_openid import exceptions
+import cgi
 
 _logger = utils.get_logger(__name__)
 
@@ -35,7 +37,8 @@ class _ClientCallbackServer(HTTPServer):
     This server will wait for a single request, storing the access_token
     obtained from the incoming request into the 'token' attribute.
     """
-    headers = {}
+
+    token = None
 
     def server_bind(self):
         """Override original bind and set a timeout.
@@ -65,11 +68,19 @@ class _ClientCallbackHandler(BaseHTTPRequestHandler):
         This method will process the query parameters and get the
         cookies from the completed mod_auth_openid session
         """
-
+        postvars = {}
         #we specifically need the mod_auth_openidc cookies but we will pass store all headers for now
 
         if self.headers:
-            self.server.headers = self.headers
+
+            form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={'REQUEST_METHOD': 'POST',
+                            'CONTENT_TYPE': self.headers['Content-Type'],
+                            })
+
+
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
@@ -78,6 +89,20 @@ class _ClientCallbackHandler(BaseHTTPRequestHandler):
                 b"<body><p>The authentication flow has been completed.</p>"
                 b"<p>You can close this window.</p>"
                 b"</body></html>")
+
+            for field in form.keys():
+                field_item = form[field]
+                if field_item.filename:
+                    file_data = field_item.file.read()
+                    file_len = len(file_data)
+                    del file_data
+                    response = '\tUploaded {} as {!r} ({} bytes\n'.format(field, field_item.filename, file_len)
+                else:
+                    # Regular Form Value
+                    postvars[field] = form[field].value
+                    response = '\t{}={}\n'.format(field, form[field].value)
+
+            self.server.token = postvars['token']
         else:
             self.send_response(501)
             self.send_header("Content-type", "text/html")
@@ -88,7 +113,7 @@ class _ClientCallbackHandler(BaseHTTPRequestHandler):
                 b"<p>You can close this window.</p>"
                 b"</body></html>")
 
-def _wait_for_session_cookies(redirect_host, redirect_port):
+def _wait_for_token(redirect_host, redirect_port):
     """Spawn an HTTP server and wait for the auth id_token.
 
     :param redirect_host: The hostname where the authorization request will
@@ -116,8 +141,8 @@ def _wait_for_session_cookies(redirect_host, redirect_port):
     httpd.handle_request()
     httpd.server_close()
 
-    if httpd.headers:
-        return httpd.headers['Cookie']
+    if httpd.token:
+        return httpd.token
     else:
         raise exceptions.MissingOidcSessionHeaders
 
@@ -208,7 +233,7 @@ class OpenIDConnect(oidc._OidcBase):
 
         return location
 
-    def _get_session_cookies(self, location):
+    def _get_access_token(self, location):
         """Spawn a browser session to continue the authentication process. The
 
         The response we get from keystone is used to create our access token
@@ -223,23 +248,26 @@ class OpenIDConnect(oidc._OidcBase):
         # Since we initiated the auth request via Keystone we will have the
         # OIDC session cookies in the headers from mod_auth_openidc.  We need
         # to complete the authentication to get the keystone token
-        cookies = _wait_for_session_cookies(self.redirect_host, self.redirect_port)
-        return cookies
+
+        token = _wait_for_token(self.redirect_host, self.redirect_port)
+        return token
 
 
-    def _get_keystone_token(self, session, cookie):
+    def _get_keystone_token(self, session, token):
         """Exchange the access_token for a keystone token
 
         :param obj session: keystoneauth1.session.Session
-        :param str cookie: the cookie headers containing the mod_auth_openidc
+        :param str token: the token headers containing the mod_auth_openidc
                             session that was completed in the previous step.
 
         Returns:
             auth_response: response to a GET that includes keystone token
         """
-        headers = {"Cookie": cookie, "Content-Type": "application/json"}
 
-        auth_response = session.get(self.federated_auth_url,
+        headers = {'X-Auth-Token': token,
+            'X-Subject-Token': token}
+
+        auth_response = session.get(self.auth_url + '/auth/tokens',
                                      headers=headers,
                                      authenticated=False)
         return auth_response
@@ -253,9 +281,12 @@ class OpenIDConnect(oidc._OidcBase):
 
         This is a multi-step process:
 
-        1.- We need to establish session cookies with mod_auth_openidc
+        1.- We need to establish the login page to open an browser to complete
+            a full openID session and retrieve an access_token from keystone.
 
-        2.- Use these session cookies to obtain our keystone token
+        2.- Use this access_token to obtain our keystone token
+
+        3.- Pass the token response to the session.create() to esablish our session
 
         :param session: a session object to send out HTTP requests.
         :type session: keystoneauth1.session.Session
@@ -269,9 +300,12 @@ class OpenIDConnect(oidc._OidcBase):
             # in order to spawn our web browser.
             location = self.get_redirect_location(session)
             # Now obtain the access token from the OIDC provider
-            cookies = self._get_session_cookies(location)
+            cookies = self._get_access_token(location) # rename to tokens
+
             response = self._get_keystone_token(session, cookies)
+
             self.auth_ref = access.create(resp=response)
+
             self.set_auth_state(self.auth_ref.auth_token)
         else:
             self.set_auth_state(cached_data)
@@ -281,7 +315,6 @@ class OpenIDConnect(oidc._OidcBase):
     def set_auth_state(self, data):
 
         if 'body' in data:
-            #json format
             jdata = data
         else:
             data = {'auth_token': data,
